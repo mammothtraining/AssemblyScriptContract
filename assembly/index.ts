03c9952b143a7dc1c980fb62be6bfcce9e9ff2a9
Mammoth Interactive INC <alexandra.mammothinteractive@gmail.com>
2022-09-08 22:11:20 +0000

    Add logging in contract

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,16 +1,20 @@
-import { storage, Context } from "near-sdk-as";
+import { storage, Context, logging } from "near-sdk-as";
 
 export class Greeting { 
 
     getGreeting(accountId: string): string | null { 
+
+        logging.log("Getting greeting for " + accountId);
+
         return storage.get<string>(accountId, 
             "Greeting not available"); 
     }
-
     setGreeting(greeting: string): void { 
 
-        storage.set(Context.sender, greeting);
+        logging.log("Setting greeting for " + Context.sender);
+        logging.log("Setting greeting as " + greeting);
 
+        storage.set(Context.sender, greeting);
     } 
 }
 
